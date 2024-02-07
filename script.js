c959d48256eea1355b0a4b16b1f00b0af3b91d3a
Henrique <kciqueks@gmail.com>
2024-02-07 17:59:38 -0300

    objetos

---
+++ script.js
@@ -0,0 +1,144 @@
+const personagemOnePiece = {
+    nome: 'Monkey D. Luffy',
+    nacionalidade: 'Brasileiro',
+    akumanomi: 'Gomu Gomu',
+    amante: 'Hancock',
+    bando: 'Chapéu de Palha',
+    recompensa: '฿ 3,000,000,000',
+    recrutamento:[{
+        arco: 'East Blue',
+        saga: 'Romance Dawn',
+        episodio: '1 e 2',
+    }],
+}
+const personagemOnePiece2 = {
+    nome:'Roronoa Zoro',
+    nacionalidade:'Japonês',
+    akumanomi:'',
+    amante:'Sake',
+    bando:'Chapéu de Palha',
+    recompensa:'฿ 1,111,000,000',
+    recrutamento:[{
+        arco:'Romance Dawn',
+        saga:'East Blue',
+        episodio:'3',
+    }]  
+}
+const personagemOnePiece3 = {
+    nome:'Jinbe',
+    nacionalidade:'Índia',
+    akumanomi:'',
+    amante:'',
+    bando:'Chapéu de Palha',
+    recompensa:'฿ 1,100,000,000',
+    recrutamento:[{
+        arco:'Whole Cake',
+        saga:'Yonkou',
+        episodio:'833',
+    }]
+}
+const personagemOnePiece4 = {
+    nome:'Sanji',
+    nacionalidade:'Francês',
+    akumanomi:'',
+    amante:'Todas',
+    bando:'Chapéu de Palha',
+    recompensa:'฿ 1,032,000,000',
+    recrutamento:[{
+        arco:'Baratie',
+        saga:'East Blue',
+        episodio:'30',
+    }]
+}
+const personagemOnePiece5 = {
+    nome:'Nico Robin',
+    nacionalidade:'Rússia',
+    akumanomi:'Hana Hana no mi',
+    amante:'Law',
+    bando:'Chapéu de Palha',
+    recompensa:'฿ 930,000,000',
+    recrutamento:[{
+        arco:'Alabasta',
+        saga:'Alabasta',
+        episodio:'92',
+    }]
+}
+const personagemOnePiece6 = {
+    nome:'God D. Usopp',
+    nacionalidade:'África',
+    akumanomi:'',
+    amante:'Mentira',
+    bando:'Chapéu de Palha',
+    recompensa:'฿ 500,000,000',
+    recrutamento:[{
+        arco:'Vila Syrup',
+        saga:'East Blue',
+        episodio:'9',
+    }]
+}
+const personagemOnePiece7 = {
+    nome:'Franky',
+    nacionalidade:'Estados Unidos',
+    akumanomi:'',
+    amante:'Cola',
+    bando:'Chapéu de Palha',
+    recompensa:'฿ 394,000,000',
+    recrutamento:[{
+        arco:'Pós-Enies Lobby',
+        saga:'Water 7',
+        episodio:'322',
+    }]
+}
+const personagemOnePiece8 = {
+    nome:'Brook',
+    nacionalidade:'Áustria',
+    akumanomi:'Yomi Yomi no Mi',
+    amante:'Laboon',
+    bando:'Chapéu de Palha',
+    recompensa:'฿ 383,000,000',
+    recrutamento:[{
+        arco:'Thriller Bark',
+        saga:'Thriller Bark',
+        episodio:'381',
+    }]
+}
+const personagemOnePiece9 = {
+    nome:'Nami',
+    nacionalidade:'Suécia',
+    akumanomi:'',
+    amante:'Dinheiro',
+    bando:'Chapéu de Palha',
+    recompensa:'฿ 1,000',
+    recrutamento:[{
+        arco:'Orange Town',
+        saga:'East Blue',
+        episodio:'6',
+    }]
+}
+const personagemOnePiece10 = {
+    nome:'Tony Tony Chopper',
+    nacionalidade:'Canadá',
+    akumanomi:'Hito Hito no Mi',
+    amante:'Algodão Doce',
+    bando:'Chapéu de Palha',
+    recompensa:'฿ 366,000,000',
+    recrutamento:[{
+        arco:'Ilha de Drum',
+        saga:'Alabasta',
+        episodio:'91',
+    }]
+}
+
+const mugiwara = [personagemOnePiece,
+personagemOnePiece2,
+personagemOnePiece3,
+personagemOnePiece4,
+personagemOnePiece5,
+personagemOnePiece6,
+personagemOnePiece7,
+personagemOnePiece8,
+personagemOnePiece9,
+personagemOnePiece10]
+
+console.log(personagemOnePiece10.recrutamento[0].arco)
+
